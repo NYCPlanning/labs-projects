68b3440cf847b31d8a61b0410886f30c86d121fb
allthesignals <wilburnforce@gmail.com>
2018-08-04 22:29:40 -0400

    Initial commit

--- app/routes/application.js
+++ app/routes/application.js
@@ -6,6 +6,7 @@ const GITHUB_USER = 'nycplanning';
 
 export default Route.extend({
   model() {
-    return fetch(`${HOST}?q=user:${GITHUB_USER}%20topic:labs`);
+    return fetch(`${HOST}?q=user:${GITHUB_USER}%20topic:labs`)
+      .then(blob => blob.json());
   }
 });
