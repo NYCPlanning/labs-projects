01eb69c763541713b08799a55d626c2f6968c304
Andy Cochran <acochran@planning.nyc.gov>
2019-01-11 15:07:11 -0500

    add issue widget

--- app/components/issue-widget.js
+++ app/components/issue-widget.js
@@ -2,5 +2,5 @@ import Component from '@ember/component';
 
 export default Component.extend({
   tagName: 'td',
-  classNames: ['issues-widget', 'leading-tight', 'text-right', 'border-t', 'border-light-gray'],
+  classNames: ['issues-widget', 'border-t', 'border-light-gray'],
 });
